f4ee63150a13380e71ef02894125c1528a6411c7
suntan-superman <sjroy5@hotmail.com>
2025-12-06 14:07:34 -0800

    style: Changed team page button colors for better distinction

--- sample/functions/src/integrations/toast/index.ts
+++
@@ -1,6 +0,0 @@
-// Toast POS Integration - Main exports
-
-export { createToastClient, ToastClient } from './toastClient';
-export { authenticateToast, disconnectToast, isToastConnected } from './toastOAuth';
-export { syncMenuFromToast, scheduleMenuSync } from './menuSync';
-export { pushOrderToToast, autoPushOrderToToast } from './orderPush';
